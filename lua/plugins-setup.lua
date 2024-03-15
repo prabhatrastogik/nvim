36289783c533e0fa7ec600a3121754b3eebe7ec0
Prabhat Rastogi <prabhat@Prabhats-MacBook-Pro.local>
2024-03-16 03:20:50 +0530

    Lazy Config Corrections 1

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -13,7 +13,7 @@ vim.opt.runtimepath:prepend(lazypath)
 
 -- load lazy
 require("lazy").setup("plugins", {
-    install = { colorscheme = { "tokyonight" } },
+    install = { colorscheme = { 'projekt0n/github-nvim-theme' } }, --"tokyonight" } },
     defaults = { lazy = true },
     ui = {
         border = "rounded",
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,8 +1,36 @@
 return {
     { "nvim-lua/plenary.nvim" },
     -- { "MunifTanjim/nui.nvim" },
-    { "nvim-tree/nvim-web-devicons", opts = { default = true } },
-    { 'akinsho/bufferline.nvim',     dependencies = 'nvim-tree/nvim-web-devicons', event = "VeryLazy", config = true, },
-    -- { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {}, event = "VeryLazy" },
-    { "lewis6991/gitsigns.nvim",     event = "VeryLazy", }
+    { "nvim-tree/nvim-web-devicons",         opts = { default = true } },
+    { 'akinsho/bufferline.nvim',             dependencies = 'nvim-tree/nvim-web-devicons', event = "VeryLazy", config = true, },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl",                                 opts = {},          event = "VeryLazy" },
+    { "lewis6991/gitsigns.nvim",             event = "VeryLazy", },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    },
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        -- event = "VeryLazy",
+        -- lazy = false,
+        config = true,
+        keys = {
+            { "<leader>xx", "<cmd>TroubleToggle<cr>" },
+            { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
+            { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>" },
+            { "<leader>xl", "<cmd>TroubleToggle loclist<cr>" },
+            { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>" },
+
+        }
+    }
 }
