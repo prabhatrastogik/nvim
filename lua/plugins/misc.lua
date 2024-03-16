26c16fef24287c4b3bc13af76ee7d918122a4af8
Prabhat Rastogi <prabhat@192.168.1.3>
2024-03-16 23:53:22 +0530

    Added GitSigns

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -4,7 +4,7 @@ return {
     { "nvim-tree/nvim-web-devicons",         opts = { default = true } },
     { 'akinsho/bufferline.nvim',             dependencies = 'nvim-tree/nvim-web-devicons', event = "VeryLazy", config = true, },
     { "lukas-reineke/indent-blankline.nvim", main = "ibl",                                 opts = {},          event = "VeryLazy" },
-    { "lewis6991/gitsigns.nvim",             event = "VeryLazy", },
+    { "lewis6991/gitsigns.nvim",             event = "VeryLazy",                           config = true, },
     {
         "folke/which-key.nvim",
         event = "VeryLazy",
