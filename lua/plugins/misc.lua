0bc025ba8a4b53e681c0a1884daf13387477ff11
Prabhat Rastogi <prabhat@Prabhats-MacBook-Pro.local>
2024-04-05 23:03:57 +0530

    Separate out misc plugins

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,8 +1,6 @@
 return {
     { "nvim-lua/plenary.nvim" },
     -- { "MunifTanjim/nui.nvim" },
-    { "nvim-tree/nvim-web-devicons",         opts = { default = true } },
-    { 'akinsho/bufferline.nvim',             dependencies = 'nvim-tree/nvim-web-devicons', event = "VeryLazy", config = true, },
-    { "lukas-reineke/indent-blankline.nvim", main = "ibl",                                 opts = {},          event = "VeryLazy" },
-    { "lewis6991/gitsigns.nvim",             event = "VeryLazy",                           config = true, },
+    { "nvim-tree/nvim-web-devicons", opts = { default = true } },
+    { 'stevearc/dressing.nvim',      event = "VeryLazy", },
 }
