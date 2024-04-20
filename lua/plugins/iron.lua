0f61480169c7bdb7c1f30f931f89072286da3aa2
Prabhat Rastogi <prabhat@Prabhats-MacBook-Pro.local>
2024-04-20 23:32:14 +0530

    Jupyter Notebooks + Iron

--- lua/plugins/iron.lua
+++ lua/plugins/iron.lua
@@ -1,13 +1,13 @@
 return {
     "Vigemus/iron.nvim",
+    cmd = { "IronRepl", "IronRestart", "IronFocus", "IronHide" },
     config = function()
         local iron = require("iron.core")
         local view = require("iron.view")
         iron.setup({
             keymaps = {
-                send_motion = "<Leader>rs",
-                visual_send = "<Leader>rs",
-                send_file = "<Leader>rf",
+                send_motion = "<Leader>rm",
+                visual_send = "<Leader>rv",
                 send_line = "<Leader>rl",
                 -- send_until_cursor = "<Leader>su",
                 -- send_mark = "<Leader>sm",
@@ -20,8 +20,16 @@ return {
                 clear = "<Leader>rc",
             },
             config = {
-                repl_open_cmd = view.split.vertical.botright(0.4)
+                -- repl_open_cmd = view.split.vertical.botright(0.4)
+                repl_open_cmd = view.center("40%", 30),
+                ignore_blank_lines = true,
             }
         })
     end,
+    keys = {
+        { '<Leader>rs', '<cmd>IronRepl<cr>' },
+        { '<Leader>rr', '<cmd>IronRestart<cr>' },
+        { '<Leader>rf', '<cmd>IronFocus<cr>' },
+        { '<Leader>rh', '<cmd>IronHide<cr>' },
+    }
 }
